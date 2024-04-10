28fb08f66d08918a833e206017d8d5124ac05e73
Yovany Sora Olaya <yovannysora@gmail.com>
2024-04-10 07:43:49 -0500

    validacion final de estructuras

--- src/mundo/Connect.java
+++ src/mundo/Connect.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Connect {
     private String url = "jdbc:mysql://localhost:3306/tienda?serverTimezone=UTC";
     private String username = "root";
-    private String password = "Sharito1977*";
+    private String password = "ingrese su clave";
 
     public Connection getConnection() throws SQLException{
         return DriverManager.getConnection(url, username, password);
